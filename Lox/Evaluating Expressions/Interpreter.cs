9e1f636b708b2a2326e2b9601b3e3e3970b20d17
Byron Mayne <ByronMayne@gmail.com>
2017-04-07 11:25:42 -0400

    7.2.0  - Evaluating Expressions
    Created the interpreter class

---
+++ Lox/Evaluating Expressions/Interpreter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static LoxLanguage.Expression;
+
+namespace LoxLanguage
+{
+    public class Interpreter : IVisitor<object>
+    {
+        public object Visit(Literal literal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(Postfix postfix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(Conditional conditional)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(Prefix prefix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(Grouping grouping)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Visit(Binary binary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
