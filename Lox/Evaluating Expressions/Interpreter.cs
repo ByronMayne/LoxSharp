ade09b038fb384dbee001e6ae57e73b80b4647b6
Byron Mayne <ByronMayne@gmail.com>
2017-04-07 11:36:18 -0400

    7.2.3 - Evaluating unary expressions

--- Lox/Evaluating Expressions/Interpreter.cs
+++ Lox/Evaluating Expressions/Interpreter.cs
@@ -22,8 +22,20 @@ namespace LoxLanguage
 
         public object Visit(Prefix prefix)
         {
-            throw new NotImplementedException();
+            object right = Evaluate(prefix.rhs);
+
+            switch(prefix.opp.type)
+            {
+                case TokenType.Bang:
+                    return !IsTrue(right);
+                case TokenType.Minus:
+                    return -(double)right;
+            }
+
+            // Unreachable
+            return null;
         }
+
 
         public object Visit(Grouping grouping)
         {
@@ -35,6 +47,20 @@ namespace LoxLanguage
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Checks if a value is true or not in Lox. Null is false and a boolean
+        /// returns it's value. Everything else is true. 
+        /// </summary>
+        private bool IsTrue(object value)
+        {
+            if (value == null) return false;
+            if (value is bool) return (bool)value;
+            return true; 
+        }
+
+        /// <summary>
+        /// Returns the value of the expression.
+        /// </summary>
         private object Evaluate(Expression expression)
         {
             return expression.Accept(this);
