e6e912d94f3dd97fe385e5a1a17403cdb6cd7174
Byron Mayne <ByronMayne@gmail.com>
2017-06-02 18:51:58 -0400

    Added a simple project to run Lox Script

---
+++ LoxRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using LoxLanguage;
+using System;
+
+namespace LoxRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lox lox = new Lox(new string[] { "D://Repositories//LoxSharp//LoxRunner//Example.Lox" });
+            lox.Run();
+            Console.ReadLine();
+        }
+    }
+}
