479bf6a87c94bc77aa4bad4eabe1099e5aca25c0
Byron Mayne <ByronMayne@gmail.com>
2017-06-02 20:04:19 -0400

    8 . 5 . 1 Nesting and shadowing

--- Lox/Environment.cs
+++ Lox/Environment.cs
@@ -5,11 +5,18 @@ namespace LoxLanguage
 {
     public class Environment
     {
+        private Environment m_Enclosing;
         private Dictionary<string, object> m_Values;
 
         public Environment()
         {
             m_Values = new Dictionary<string, object>();
+            m_Enclosing = null;
+        }
+
+        public Environment(Environment enclosing) : this()
+        {
+            m_Enclosing = enclosing;
         }
 
         public void Define(string name, object value)
@@ -23,6 +30,9 @@ namespace LoxLanguage
             {
                 return m_Values[name.lexeme];
             }
+
+            if (m_Enclosing != null) return m_Enclosing.Get(name); 
+
             throw new RuntimeError(name, "Undefined variable '" + name.lexeme + ".");
         }
 
@@ -33,6 +43,13 @@ namespace LoxLanguage
                 m_Values[name.lexeme] = value;
                 return;
             }
+
+            if(m_Enclosing != null)
+            {
+                m_Enclosing.Assign(name, value);
+                return;
+            }
+
             throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
         }
     }
